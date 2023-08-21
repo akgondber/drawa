70f03ae7e56c9ded8f76175c9f86cf96977dbbb6
Rushan Alyautdinov <akgondber@gmail.com>
2023-08-21 20:31:39 +0300

    Init

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const { Survey } = require('enquirer');
+
+const prompt = new Survey({
+  name: 'experience',
+  message: 'Please rate your experience',
+   scale: [
+    { name: '1', message: 'Strongly Disagree' },
+    { name: '2', message: 'Disagree' },
+    { name: '3', message: 'Neutral' },
+    { name: '4', message: 'Agree' },
+    { name: '5', message: 'Strongly Agree' }
+  ],
+  margin: [0, 0, 2, 1],
+  choices: [
+    {
+      name: 'interface',
+      message: 'The website has a friendly interface.'
+    },
+    {
+      name: 'navigation',
+      message: 'The website is easy to navigate.'
+    },
+    {
+      name: 'images',
+      message: 'The website usually has good images.'
+    },
+    {
+      name: 'upload',
+      message: 'The website makes it easy to upload images.'
+    },
+    {
+      name: 'colors',
+      message: 'The website has a pleasing color palette.'
+    }
+  ]
+});
+
+prompt.run()
+  .then(value => console.log('ANSWERS:', value))
+  .catch(console.error);
+  
 console.log("Hello, World!");
