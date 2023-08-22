75d10d3969468061f4d8b5243ddf1042829f1cc7
Rushan Alyautdinov <akgondber@gmail.com>
2023-08-22 23:43:23 +0300

    Init

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const prompt = new Survey({
     { name: '2', message: 'Disagree' },
     { name: '3', message: 'Neutral' },
     { name: '4', message: 'Agree' },
-    { name: '5', message: 'Strongly Agreee' }
+    { name: '5', message: 'Strongly Agreeeee' }
   ],
   margin: [0, 0, 2, 1],
   choices: [
